fdabf79cef01a997f80f3d3a34b60d26d50acaed
Sam Clarke <cronneloctopus@gmail.com>
2013-03-29 08:02:53 -0700

    update heroku settings

--- application.py
+++ application.py
@@ -8,11 +8,11 @@ from mongoengine import connect
 app = Flask(__name__)
 app.debug = True
 
+"""
 app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
-
-
 """
+
 app.config["MONGODB_DB"] = 'app14198794'
 connect(
     'app14198794',
@@ -21,8 +21,6 @@ connect(
     host='mongodb://heroku:a614e68b445d0d9d1c375740781073b4@alex.mongohq.com:10095/app14198794',
     port=10095
 )
-"""
-
 
 db = MongoEngine(app)
 
