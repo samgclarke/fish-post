9c23992eeaee53f0a342dbda92f2bb12b6e72777
Sam Clarke <cronneloctopus@gmail.com>
2013-03-29 11:46:53 -0700

    add manager

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import datetime
 from flask import Flask, url_for
 from flask.ext.mongoengine import MongoEngine, mongoengine
 from mongoengine import connect
+from flask.ext.script import Manager, Server
 
 from config import app
 
@@ -20,4 +21,12 @@ db = MongoEngine(app)
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # Turn on debugger by default and reloader
+    manager = Manager(app)
+    manager.add_command("runserver", Server(
+        use_debugger = True,
+        use_reloader = True,
+        host = '0.0.0.0',
+        port=port)
+    )
+    manager.run()
