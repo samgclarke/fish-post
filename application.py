59c793f3532929a9d1783919c4c6a8726c577346
Sam Clarke <cronneloctopus@gmail.com>
2013-03-29 07:59:57 -0700

    add posts, comments models

--- application.py
+++ application.py
@@ -1,32 +1,84 @@
 #!/usr/bin/env python
 import os
 import datetime
-from flask import Flask
+from flask import Flask, url_for
 from flask.ext.mongoengine import MongoEngine, mongoengine
 from mongoengine import connect
 
-
 app = Flask(__name__)
-app.config["MONGODB_DB"] = ''
+app.debug = True
+
+app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+
+"""
+app.config["MONGODB_DB"] = 'app14198794'
 connect(
-    '',
+    'app14198794',
     username='heroku',
-    password='',
-    host='',
-    port=
+    password='a614e68b445d0d9d1c375740781073b4',
+    host='mongodb://heroku:a614e68b445d0d9d1c375740781073b4@alex.mongohq.com:10095/app14198794',
+    port=10095
 )
+"""
+
+
 db = MongoEngine(app)
 
 
 class Post(db.DynamicDocument):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-    body = db.StringField(max_length=255, required=True)
+    title = db.StringField(max_length=255, required=True)
+    slug = db.StringField(max_length=255, required=True)
+    comments = db.ListField(db.EmbeddedDocumentField('Comment'))
+
+    def get_absolute_url(self):
+        return url_for('post', kwargs={"slug": self.slug})
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def post_type(self):
+        return self.__class__.__name__
+
+    meta = {
+        'allow_inheritance': True,
+        'indexes': ['-created_at', 'slug'],
+        'ordering': ['-created_at']
+    }
+
+
+class BlogPost(Post):
+    body = db.StringField(required=True)
+
+
+class Video(Post):
+    embed_code = db.StringField(required=True)
+
+
+class Image(Post):
+    image_url = db.StringField(required=True, max_length=255)
+
+
+class Quote(Post):
+    body = db.StringField(required=True)
+    author = db.StringField(
+        verbose_name="Author Name", required=True, max_length=255
+    )
+
+
+class Comment(db.EmbeddedDocument):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    body = db.StringField(verbose_name="Comment", required=True)
+    author = db.StringField(verbose_name="Name", max_length=255, required=True)
 
 
 @app.route('/')
 def hello_world():
-    posts = Post.objects.all()
-    return 'Hello Mongo! Read this: ' + posts[0].body
+    return 'Hello Mongo: '
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
