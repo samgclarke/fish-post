c995a0c24f4271958343f2a0aab20046cd74fa05
Sam Clarke <sam@rivalschools.com>
2013-10-30 16:41:49 -0700

    update password

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ app.config["MONGODB_DB"] = 'app14198794'
 connect(
     'app14198794',
     username='heroku',
-    password='a614e68b445d0d9d1c375740781073b4',
-    host='mongodb://heroku:a614e68b445d0d9d1c375740781073b4@alex.mongohq.com:10095/app14198794',
+    password='3_G<=dcuX1W7oH=',
+    host='mongodb://heroku:3_G<=dcuX1W7oH=@alex.mongohq.com:10095/app14198794',
     port=10095
 )
 
